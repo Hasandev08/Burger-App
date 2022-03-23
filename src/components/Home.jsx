a963757eb409b211c312b520a53c2b2c6828c44f
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-03-24 02:41:44 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [values, setValues] = useState([
+    {
+      name: "Lettuce",
+    },
+    {
+      name: "Bacon",
+    },
+    {
+      name: "Cheese",
+    },
+    {
+      name: "Meat",
+    },
+  ]);
+
+  return (
+    <div className="burger-app">
+      <div className="Burger">
+        <div className="Burger-Top"></div>
+        <p>No Ingredients Added</p>
+        <div className="Burger-Bottom"></div>
+      </div>
+      <div className="app-functions">
+        <p>
+          Current Price: <b>$3.00</b>
+        </p>
+        <div className="App-ingredient">
+          {values.map((item, index) => (
+            <div key={index.toString()}>
+              <div>
+                <div className="ingredient">
+                  <p>{item.name}</p>
+                </div>
+                <div className="less">
+                  <button>Less</button>
+                </div>
+                <div className="more">
+                  <button>More</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button className="sign-up" disabled>
+          SIGN UP TO ORDER
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
