32b3ed810b0b3fe7c505e1558ad3c0eed995d846
Hasan Ahmed <92812904+HasanAhmed987@users.noreply.github.com>
2022-04-06 16:13:19 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,40 +1,54 @@
 import React, { useState } from "react";
-import Burger from "./Burger";
-import { NavLink } from "react-router-dom";
+import Burger from "./common/Burger";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
-const Home = () => {
-  const values = [
+const Home = ({ user }) => {
+  const [values, setValues] = useState([
     {
       name: "Lettuce",
       price: 0.5,
+      quantity: 0,
     },
     {
       name: "Bacon",
       price: 0.7,
+      quantity: 0,
     },
     {
       name: "Cheese",
       price: 0.4,
+      quantity: 0,
     },
     {
       name: "Meat",
       price: 1.3,
+      quantity: 0,
     },
-  ];
+  ]);
 
   const [price, setPrice] = useState(3);
 
-  const handleAddition = (ingrPrice) => setPrice(price + ingrPrice);
-  const handleSubtraction = (ingrPrice) => setPrice(price - ingrPrice);
+  const handleAddition = (index) => {
+    const tempValues = [...values]
+    tempValues[index].quantity = tempValues[index].quantity + 1
+    setValues(tempValues)
+    setPrice(price + tempValues[index].price)
+  }
+  const handleSubtraction = (index) => {
+    const tempValues = [...values]
+    tempValues[index].quantity = tempValues[index].quantity - 1
+    setValues(tempValues)
+    setPrice(price - tempValues[index].price)
+  }
 
   return (
     <div className="burger-app">
-      <Burger />
+      <Burger values={values} />
       <div className="app-functions">
         <p>
           <div>
-            Current Price: <b>${price}</b>
+            Current Price: <b>${price.toFixed(2)}</b>
           </div>
         </p>
         <div className="App-ingredient">
@@ -52,7 +66,7 @@ const Home = () => {
                   </button>
                 </div>
                 <div className="more">
-                  <button onClick={() => handleAddition(item.price)}>
+                  <button onClick={() => handleAddition(index)}>
                     More
                   </button>
                 </div>
@@ -61,7 +75,10 @@ const Home = () => {
           ))}
         </div>
         <button className="sign-up">
-          <NavLink className="nav-link" to="/signup">SIGN UP TO ORDER</NavLink>
+
+          {user ? <Link className="link" to="/signup">ORDER NOW</Link>
+            : <Link className="link" to="/signup">SIGN UP TO ORDER</Link>}
+
         </button>
       </div>
     </div>
