c912e3d9acef6237508c538fb17fa90224872b42
Hasan Ahmed <92812904+HasanAhmed987@users.noreply.github.com>
2022-04-06 16:15:36 +0500

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-import Burger from "./Burger";
+import React, { useState } from "react"
+import Burger from "./common/Burger"
+import { Link } from "react-router-dom"
+import SignupForm from "./SignupForm"
+import "./Signup.css"
 
 const Signup = () => {
+
+    const [showForm, setShowForm] = useState(false)
+
     return (
-        <Burger />
+        <div className="signup">
+            <h1>We hope it tastes well!</h1>
+            <Burger />
+            <div className="buttons">
+                <button className="Danger">
+                    <Link className="danger-button" to="/">Cancel</Link>
+                </button>
+                <button onClick={() => setShowForm(true)} className="Success">Continue</button>
+
+                {
+                    showForm ? <SignupForm /> : null
+                }
+
+            </div>
+        </div>
     );
 }
 
